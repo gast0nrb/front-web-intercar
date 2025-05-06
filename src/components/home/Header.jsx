c3e1f9646a5ac92fb30b4f53177ffb8267411d17
gast0nrb <developer.tongas@gmail.com>
2025-05-06 18:16:14 -0400

    Agregado un fetch

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -1,6 +1,6 @@
 const Nav = () => {
   return (
-    <header class="text-center">
+    <header class="text-center bg-neutral-100">
       <div class="md:py-5 xs:py-3 flex justify-between px-5 gap-2">
         <a className="w-20" href="/"><img src="/logo.png" alt="" className="md:w-20 xs:w-12"/></a>
         <h1 className="font-extrabold flex tracking-widest xs:text-md md:text-2xl xl:text-3xl my-auto text-orange-400 xs:text-shadow-zinc-300 md:text-shadow-purple-100 text-shadow-lg"><span className="xs:hidden xl:block mx-1">VENTA DE </span>ACCESORIOS <span className="mx-1 xs:hidden xl:block">Y REPUESTOS</span> AUTOMOTRICES</h1>
--- src/components/home/WrapSucursal.jsx
+++ src/components/home/WrapSucursal.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Sucursal from "./Sucursal";
 
 const data = [
@@ -81,38 +81,55 @@ const data = [
 
 const WrapSucursal = () => {
   const [display, setDisplay] = useState(-1)
+  const [sucursales, setSucursales] = useState([])
+  const [error, setError] = useState("")
+  useEffect(() => {
+    (async function () {
+      try {
+        const fetchSucursales = await fetch("http://localhost:8080/api/intercar/branch?page=0", {
+          method: 'GET',
+          mode: 'cors'
+        })
+        const jsonSucursales = await fetchSucursales.json()
+        console.log(jsonSucursales.data)
+        setSucursales([...jsonSucursales.data])
+      } catch (err) {
+        setError("No se encontraron sucursales");
+      }
+    })();
+  }, [])
   return (
     <section className="w-11/12 mx-auto my-20" id="sucursales">
       <h2 className="text-neutral-100 tracking-widest rounded-sm border-x-4 border-orange-500 px-2 bg-neutral-900 w-fit mx-auto">
         SUCURSALES
       </h2>
-      <h3 className="font-bold">Sucursales en santiago</h3>
+      <h3 className="font-bold">Sucursales en Santiago</h3>
       <div className="grid xs:grid-cols-2 md:grid-cols-4 gap-2">
-        {data
-          .filter((f) => f.District.City.name.toLowerCase() == "santiago")
+        {sucursales
+          .filter((s) => s.DISTRICT.CITY.name.toLowerCase() == "santiago")
           .map((sucursal) => (
             <Sucursal
               display={display}
               setDisplay={setDisplay}
               id={sucursal.id}
-              title={sucursal.title}
-              addres={sucursal.addres}
+              title={sucursal.name}
+              addres={sucursal.adress}
               phone={sucursal.phone}
             />
           ))}
       </div>
       <h3 className="font-bold mt-5">Sucursales en regiones</h3>
       <div className="grid xs:grid-cols-2 md:grid-cols-4 gap-2">
-        {data
-          .filter((f) => f.District.City.name.toLowerCase() != "santiago")
+        {sucursales
+          .filter((f) => f.DISTRICT.CITY.name.toLowerCase() != "santiago")
           .map((sucursal) => (
             <Sucursal
               display={display}
               setDisplay={setDisplay}
               id={sucursal.id}
-              title={sucursal.title}
+              title={sucursal.name}
               phone={sucursal.phone}
-              addres={sucursal.addres}
+              addres={sucursal.adress}
             />
           ))}
       </div>
