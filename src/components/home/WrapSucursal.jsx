bd668f0cfc773cf6eb339a76e610a06bf48a2c47
gast0nrb <developer.tongas@gmail.com>
2025-08-19 12:31:20 -0400

    Lista la página web ?

--- src/components/home/WrapSucursal.jsx
+++ src/components/home/WrapSucursal.jsx
@@ -21,39 +21,54 @@ const WrapSucursal = () => {
   }, [])
   return (
     <section className="w-11/12 mx-auto my-20" id="sucursales">
-      <h2 className="text-neutral-200 tracking-widest rounded-sm border-x-4 border-orange-500 px-2 bg-neutral-900 w-fit mx-auto text-lg font-semibold mb-3">
-        SUCURSALES
-      </h2>
-      <h3 className="font-bold">Sucursales en Santiago</h3>
-      <div className="grid xs:grid-cols-2 md:grid-cols-4 gap-2">
-        {sucursales
-          .filter((s) => s.DISTRICT.CITY.name.toLowerCase() == "santiago")
-          .map((sucursal) => (
-            <Sucursal
-              display={display}
-              setDisplay={setDisplay}
-              id={sucursal.id}
-              title={sucursal.name}
-              addres={sucursal.adress}
-              phone={sucursal.phone}
-            />
-          ))}
-      </div>
-      <h3 className="font-bold mt-5">Sucursales en regiones</h3>
-      <div className="grid xs:grid-cols-2 md:grid-cols-4 gap-2">
-        {sucursales
-          .filter((f) => f.DISTRICT.CITY.name.toLowerCase() != "santiago")
-          .map((sucursal) => (
-            <Sucursal
-              display={display}
-              setDisplay={setDisplay}
-              id={sucursal.id}
-              title={sucursal.name}
-              phone={sucursal.phone}
-              addres={sucursal.adress}
-            />
-          ))}
-      </div>
+      {
+        (error || sucursales.length == 0) ? (
+          <>
+            <h2 className="text-neutral-200 tracking-widest rounded-sm border-x-4 border-orange-500 px-2 bg-neutral-900 w-fit mx-auto text-lg font-semibold mb-3">
+              SUCURSALES
+            </h2>
+            <h3 className="font-bold">Sucursales en Santiago</h3>
+            <div className="grid xs:grid-cols-2 md:grid-cols-4 gap-2">
+              {sucursales
+                .filter((s) => s.DISTRICT.CITY.name.toLowerCase() == "santiago")
+                .map((sucursal) => (
+                  <Sucursal
+                    display={display}
+                    setDisplay={setDisplay}
+                    id={sucursal.id}
+                    title={sucursal.name}
+                    addres={sucursal.adress}
+                    phone={sucursal.phone}
+                  />
+                ))}
+            </div>
+            <h3 className="font-bold mt-5">Sucursales en regiones</h3>
+            <div className="grid xs:grid-cols-2 md:grid-cols-4 gap-2">
+              {sucursales
+                .filter((f) => f.DISTRICT.CITY.name.toLowerCase() != "santiago")
+                .map((sucursal) => (
+                  <Sucursal
+                    display={display}
+                    setDisplay={setDisplay}
+                    id={sucursal.id}
+                    title={sucursal.name}
+                    phone={sucursal.phone}
+                    addres={sucursal.adress}
+                  />
+                ))}
+            </div>
+          </>
+        )
+          :
+          (
+            <>
+              <h2 className="text-neutral-200 tracking-widest rounded-sm border-x-4 border-orange-500 px-2 bg-neutral-900 w-fit mx-auto text-lg font-semibold mb-3">
+                Sucursales
+              </h2>
+              <p className="text-center text-zinc-500">Problemas para obtener las sucursales</p>
+            </>
+          )
+      }
     </section>
   );
 };
