669e7fc208482bea3cb51e47028c140870b1052c
gast0nrb <developer.tongas@gmail.com>
2025-08-19 12:51:49 -0400

    listo el header

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -1,17 +1,18 @@
 const Nav = () => {
   return (
-    <header class="text-center bg-neutral-100">
-      <div class="md:py-5 xs:py-3 flex justify-between px-5 gap-2">
-        <a className="w-20" href="/"><img src="/logo.png" alt="" className="md:w-20 xs:w-12" /></a>
+    <header class="text-center bg-neutral-100 mx-auto w-full">
+      <div class="md:py-5 xs:py-3 flex md:justify-between items-center xs:justify-center md:px-5">
+
+      <a href="/"><img src="/logo.png" alt="" className="md:w-20 xs:w-12 mx-auto" /></a>
         <div class="text-center my-auto">
-<h1 class="font-[Rajdhani] block xs:text-3xl md:text-5xl lg:text-6xl font-extrabold uppercase tracking-wide xs:text-zinc-800 text-shadow-xs text-shadow-orange-300">
-INTERCAR CHILE SPA
-</h1>
-<p class="font-[Rajdhani] block text-xs my-auto font-extrabold uppercase tracking-wide text-zinc-700 xs:text-shadow-xs md:text-lg  text-shadow-orange-200">
-    VENTAS DE ACCESORIOS y REPUESTOS AUTOMOTRICES
-</p>
+          <h1 class="font-[Rajdhani] block xs:text-3xl md:text-5xl lg:text-6xl font-extrabold uppercase tracking-wide xs:text-zinc-800 text-shadow-xs text-shadow-orange-300 text-nowrap">
+            INTERCAR CHILE SPA
+          </h1>
+          <p class="font-[Rajdhani] block text-xs my-auto font-extrabold uppercase text-zinc-700 xs:text-shadow-xs md:text-lg  text-shadow-orange-200 text-nowrap">
+            VENTA DE ACCESORIOS AUTOMOTRICES
+          </p>
         </div>
-        <a className="w-20" href="/"><img src="/logo.png" alt="" className="md:w-20 xs:w-12" /></a>
+      <a href="/"><img src="/logo.png" alt="" className="md:w-20 xs:w-12 mx-auto" /></a>
       </div>
     </header>
   );
